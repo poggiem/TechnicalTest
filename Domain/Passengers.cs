b8fbdc961a9627a842ee1919bf66bc36a00f4082
manupoggi <91726224+manupoggi@users.noreply.github.com>
2023-02-16 17:49:53 +0000

    domain refactoring and testing

--- Domain/Passengers.cs
+++ Domain/Passengers.cs
@@ -1,10 +1,42 @@
 ﻿namespace Domain
 {
-    public class Car
-    {
-        int _maxPassengers = 4;
-        public bool CarRunning { get; private set; }
 
+    //use of an abstract vehicle class to implement the behavior 
+    //kept encapsulation of _running with a different access property for every different kind of vehicle ( not necessary but neither anything wrong with it) 
+    public class Car : Vehicle
+    {
+        public bool CarRunning { get { return base._running; } }
+        public Car()
+        {
+            base._maxPassengers = 4;
+        }
+
+    }
+
+    public class MotorBike : Vehicle
+    {
+        public bool MotorBikeRunning { get { return base._running; } }
+        public MotorBike()
+        {
+            base._maxPassengers = 2;
+        }
+
+    }
+
+    public class Coach : Vehicle
+    {
+        public bool CoachRunning { get { return base._running; } }
+        public Coach()
+        {
+            base._maxPassengers = 25;
+        }
+
+    }
+
+    public abstract class Vehicle
+    {
+        protected int _maxPassengers { get; set; }
+        public bool _running { get; private set; }
         public void Start(List<Person> passengers)
         {
             if (passengers.Count() > _maxPassengers)
@@ -12,44 +44,10 @@
                 throw new Exception("Too many passengers");
             }
 
-            CarRunning = true;
-        }
-    }
-
-    public class MotorBike
-    {
-        int _maxPassengers = 2;
-        public bool MotorBikeRunning { get; private set; }
-
-        public void Start(List<Person> passengers)
-        {
-            if (passengers.Count() > _maxPassengers)
-            {
-                throw new Exception("Too many passengers");
-            }
-
-            MotorBikeRunning = true;
-        }
-    }
-
-    public class Coach
-    {
-        int _maxPassengers = 25;
-        public bool CoachRunning { get; private set; }
-
-        public void Start(List<Person> passengers)
-        {
-            if (passengers.Count() > _maxPassengers)
-            {
-                throw new Exception("Too many passengers");
-            }
-
-            CoachRunning = true;
+            _running = true;
         }
 
     }
-
-
     public class Person
     {
 
--- TestProject/DomainTests.cs
+++ TestProject/DomainTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,67 @@ namespace TestProject
         public void TestsAreWorking()
         {
             Assert.Equal(1, 1);
+        }
+        //the tests are made with the assumption that the vehicle should start just with a right amount of passengers,
+        //improvements : Assert.Throws<Exception>(() => car.Start()); for the various scenarios 
+        [Fact]
+        public void Car_InputIs1_ReturnTrue()
+        {
+            var car = new Car();
+            car.Start(genPeopleArray(1));
+
+            Assert.True(car.CarRunning);
+        }
+        [Fact]
+        public void Car_InputIs0_ReturnTrue()
+        {
+            var car = new Car();
+            car.Start(genPeopleArray(0));
+
+            Assert.False(car.CarRunning);
+        }
+        [Fact]
+        public void Car_InputIs4_ReturnTrue()
+        {
+            var car = new Car();
+            car.Start(genPeopleArray(4));
+
+            Assert.True(car.CarRunning);
+        }
+        [Fact]
+        public void Car_InputIs5_ReturnFalse()
+        {
+            var car = new Car();
+            car.Start(genPeopleArray(5));
+
+            Assert.False(car.CarRunning);
+        }
+        [Fact]
+        public void Car_InputIsNeg_ReturnFalse()
+        {
+            var car = new Car();
+            car.Start(genPeopleArray(-1));
+
+            Assert.False(car.CarRunning);
+        }
+        [Fact]
+        public void Car_InputIsBig_ReturnFalse()
+        {
+            var car = new Car();
+            car.Start(genPeopleArray(514748005));
+
+            Assert.False(car.CarRunning);
+        }
+        //Can be improved as it's repeating this code that already exist in the api. This kind of utility code would find a better place in a service. 
+        private List<Person> genPeopleArray(int number)
+        {
+            var people = new List<Person>();
+
+            for (int i = 0; i < number; i++)
+            {
+                people.Add(new Person());
+            }
+            return people;
         }
     }
 }
